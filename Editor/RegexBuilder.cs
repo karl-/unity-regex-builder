810df2673156239c60cdcb77361401d112c88e06
karl- <karlh07@gmail.com>
2015-12-29 10:25:43 -0500

    Don't bother changing font in git trunk version

--- Editor/RegexBuilder.cs
+++ Editor/RegexBuilder.cs
@@ -15,21 +15,21 @@ namespace Parabox.RegexConstructor
 
 		public string pattern = "_TEMP_UV3_CHANNEL\\s\\(\"TEMP_UV3_CHANNEL\", Vector\\)\\s=\\s\\([\\d*\\.?\\d*,?]*\\)";
 		public string sample = "Shader \"Polybrush/TextureBlender\" {\n Properties {\n _MainTex (\"Base Color\", 2D) = \"white\" {}\n _TextureG (\"TextureG\", 2D) = \"white\" {}\n _TextureB (\"TextureB\", 2D) = \"white\" {}\n _TextureA (\"TextureA\", 2D) = \"white\" {}\n _Metallic (\"Metallic\", Range(0, 1)) = 0\n _Gloss (\"Gloss\", Range(0, 1)) = 0.8\n _TextureU (\"TextureU\", 2D) = \"white\" {}\n _TextureV (\"TextureV\", 2D) = \"white\" {}\n _TextureS (\"TextureS\", 2D) = \"white\" {}\n _TextureT (\"TextureT\", 2D) = \"white\" {}\n _TEMP_UV3_CHANNEL (\"TEMP_UV3_CHANNEL\", Vector) = (1,0,0,0)\n [HideInInspector]_Cutoff (\"Alpha cutoff\", Range(0,1)) = 0.5\n }\n SubShader {";
-		private Font font;
+		// private Font font;
 		private string escaped_pattern = "";
 		public Vector2 scroll = Vector2.zero;
 
 		void OnEnable()
 		{
 			escaped_pattern = pattern;
-			font = Resources.Load<Font>("monkey");
+			// font = Resources.Load<Font>("monkey");
 		}
 
 		void OnGUI()
 		{
-			Font old = GUI.skin.font;
-			GUI.skin.font = font;
-			EditorStyles.boldLabel.font = font;
+			// Font old = GUI.skin.font;
+			// GUI.skin.font = font;
+			// EditorStyles.boldLabel.font = font;
 
 			GUILayout.BeginHorizontal();
 				GUILayout.Label("Pattern", EditorStyles.boldLabel);
@@ -77,8 +77,8 @@ namespace Parabox.RegexConstructor
 			sample = EditorGUILayout.TextArea(sample, GUILayout.MinHeight(128), GUILayout.MaxHeight(128));
 
 			GUI.color = color;
-			EditorStyles.boldLabel.font = old;
-			GUI.skin.font = old;
+			// EditorStyles.boldLabel.font = old;
+			// GUI.skin.font = old;
 		}
 	}
 }
