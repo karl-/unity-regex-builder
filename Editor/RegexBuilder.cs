3eb36a3968ece2fa36e2235802d27bd3ccbe9cf7
Karl Henkel <karlh07@gmail.com>
2018-01-29 15:35:54 -0500

    fix textmeshgenerator warnings. respect multi-line flag. add a flexible splitter for search text. serialize local vars so domain reloads don't reset search text or pattern

--- Editor/RegexBuilder.cs
+++ Editor/RegexBuilder.cs
@@ -1,8 +1,13 @@
-﻿using UnityEngine;
+﻿// #define CUSTOM_FONT
+
+using System;
+using System.Globalization;
+using UnityEngine;
 using UnityEditor;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Linq;
+using System.Reflection;
+using UnityEngine.UI;
 
 namespace Parabox.RegexConstructor
 {
@@ -22,46 +27,105 @@ namespace Parabox.RegexConstructor
 			}
 		}
 
+		object[] m_SplitterArgs;
+		MethodInfo m_BeginVerticalSplit;
+		MethodInfo m_EndVerticalSplit;
+		[SerializeField] object m_SplitterState;
+		[SerializeField] string m_RegexPattern = "(?i)lorem";
+		[SerializeField] string m_SampleText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pulvinar, diam ut mattis dictum, risus elit accumsan velit, a efficitur velit mauris id massa. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque lobortis odio quis turpis bibendum malesuada. Suspendisse tincidunt molestie tortor non bibendum. Donec in lorem quis nunc pellentesque elementum sed ut arcu. Quisque ac tortor dolor. Nunc sed aliquet massa. Etiam sed orci et orci imperdiet scelerisque non ut neque. Quisque congue risus diam, quis tempor ligula pulvinar nec. Quisque blandit, tellus ut volutpat malesuada, purus dui porttitor nisi, sed egestas libero ipsum eget risus.";
+		GUIStyle m_PatternSearchBox;
+		GUIStyle m_VerticalWrapperStyle;
+
+#if CUSTOM_FONT
+		Font font;
+		const string k_FontPath = "Assets/Debug/Font/ProggyClean.ttf";
+#endif
+		GUIStyle m_MatchesStyle;
+		GUIStyle m_SampleTextStyle;
+		GUIStyle m_PatternStyle;
+		bool m_IsGuiInitialized;
+
+		[SerializeField] Vector2 m_Scroll;
+		[SerializeField] Vector2 m_ContentsScroll;
+		[SerializeField] ProcessedText m_ProcessedText;
+		[SerializeField] RegexOptions m_RegexOptions = RegexOptions.None;
+
 		[MenuItem("Window/Regex Builder Window")]
 		static void Init()
 		{
-			EditorWindow.GetWindow<RegexBuilder>(true, "Parabox Regex", true).Show();
+			GetWindow<RegexBuilder>(true, "Parabox Regex", true).Show();
 		}
-
-		public string pattern = "(?i)lorem";
-		public string sample = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pulvinar, diam ut mattis dictum, risus elit accumsan velit, a efficitur velit mauris id massa. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque lobortis odio quis turpis bibendum malesuada. Suspendisse tincidunt molestie tortor non bibendum. Donec in lorem quis nunc pellentesque elementum sed ut arcu. Quisque ac tortor dolor. Nunc sed aliquet massa. Etiam sed orci et orci imperdiet scelerisque non ut neque. Quisque congue risus diam, quis tempor ligula pulvinar nec. Quisque blandit, tellus ut volutpat malesuada, purus dui porttitor nisi, sed egestas libero ipsum eget risus.";
-#if CUSTOM_FONT
-		private Font font;
-#endif
-		public Vector2 scroll = Vector2.zero;
-		private GUIStyle matches_style = new GUIStyle();
-		private GUIStyle pattern_style = null;
-		private ProcessedText processed = new ProcessedText();
-		private bool guiInitialized = false;
 
 		void OnEnable()
 		{
-			processed = DoRegex(sample, pattern);
+			m_IsGuiInitialized = false;
+			m_ProcessedText = DoRegex(m_SampleText, m_RegexPattern, m_RegexOptions);
+
+			Type splitterStateType = Type.GetType("UnityEditor.SplitterState, UnityEditor, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null");
+
+			m_SplitterState = Activator.CreateInstance(
+				splitterStateType,
+				BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance,
+				null,
+				new object[] { new float[] { .7f, .3f }, new int[] { 120, 22 }, null },
+				CultureInfo.CurrentCulture,
+				null);
+
+			Type splitterGuiLayoutType = Type.GetType("UnityEditor.SplitterGUILayout, UnityEditor, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null");
+
+			m_BeginVerticalSplit = splitterGuiLayoutType.GetMethod(
+				"BeginVerticalSplit",
+				BindingFlags.Static | BindingFlags.Public,
+				null,
+				CallingConventions.Any,
+				new Type[] { splitterStateType, typeof(GUILayoutOption[]) },
+				null);
+
+			m_EndVerticalSplit = splitterGuiLayoutType.GetMethod(
+				"EndVerticalSplit",
+				BindingFlags.Static | BindingFlags.Public);
+
+			m_SplitterArgs = new object[]
+			{
+				m_SplitterState,
+				new GUILayoutOption[]
+				{
+					GUILayout.ExpandWidth(true),
+					GUILayout.ExpandHeight(true)
+				}
+			};
 		}
 
 		void OnGUI()
 		{
-			if(!guiInitialized)	
+			if(!m_IsGuiInitialized)
 			{
-				guiInitialized = true;
+				m_IsGuiInitialized = true;
 
+				m_MatchesStyle = new GUIStyle();
 #if CUSTOM_FONT
-				font = Resources.Load<Font>("monkey");
-				matches_style.font = font;
+				font = AssetDatabase.LoadAssetAtPath<Font>(k_FontPath);
+				m_MatchesStyle.font = font;
 #endif
-				matches_style.richText = true;
-				matches_style.margin = new RectOffset(4,4,4,4);
-				matches_style.padding = new RectOffset(2,2,2,2);
-				matches_style.normal.textColor = new Color(1f, 1f, 1f, .7f);
-				matches_style.wordWrap = true;
+				m_MatchesStyle.richText = true;
+				m_MatchesStyle.margin = new RectOffset(4,4,4,4);
+				m_MatchesStyle.padding = new RectOffset(2,2,2,2);
+				m_MatchesStyle.normal.textColor = new Color(1f, 1f, 1f, .7f);
+				m_MatchesStyle.wordWrap = true;
 
-				pattern_style = new GUIStyle(EditorStyles.textArea);
-				pattern_style.padding = new RectOffset(8,8,8,8);
+				m_PatternStyle = new GUIStyle(EditorStyles.textArea);
+				m_PatternStyle.padding = new RectOffset(8,8,8,8);
+
+				m_PatternSearchBox = new GUIStyle(EditorStyles.textField);
+				m_PatternSearchBox.fixedHeight = 20;
+				m_PatternSearchBox.margin.top += 2;
+				m_PatternSearchBox.alignment = TextAnchor.MiddleLeft;
+
+				m_SampleTextStyle = new GUIStyle(EditorStyles.textField);
+				m_SampleTextStyle.wordWrap = true;
+				m_SampleTextStyle.padding = new RectOffset(4, 4, 6, 6);
+
+				m_VerticalWrapperStyle = new GUIStyle("CN Box");
 			}
 
 #if CUSTOM_FONT
@@ -69,64 +133,95 @@ namespace Parabox.RegexConstructor
 			GUI.skin.font = font;
 			EditorStyles.boldLabel.font = font;
 #endif
+			m_BeginVerticalSplit.Invoke(null, m_SplitterArgs);
 
+			GUILayout.BeginVertical(m_VerticalWrapperStyle);
+				DoSearchAndResultsGui();
+			GUILayout.EndVertical();
+
+			GUILayout.BeginVertical(m_VerticalWrapperStyle);
+				DoContentsAndSettingsGui();
+			GUILayout.EndVertical();
+
+#if CUSTOM_FONT
+			EditorStyles.boldLabel.font = old;
+			GUI.skin.font = old;
+#endif
+			m_EndVerticalSplit.Invoke(null, null);
+		}
+
+		void DoSearchAndResultsGui()
+		{
 			GUILayout.BeginHorizontal();
-				GUILayout.Label("Pattern", EditorStyles.boldLabel);
-				GUILayout.FlexibleSpace();
-				if(GUILayout.Button("cheatsheet"))	
-					Application.OpenURL("https://msdn.microsoft.com/en-us/library/az24scfc(v=vs.110).aspx");
-				if(GUILayout.Button("copy escaped pattern"))
-					GUIUtility.systemCopyBuffer = pattern.Replace("\\", "\\\\").Replace("\"", "\\\"");
+
+			GUILayout.Label("Pattern", EditorStyles.boldLabel);
+			GUILayout.FlexibleSpace();
+			if(GUILayout.Button("cheatsheet"))
+				Application.OpenURL("https://msdn.microsoft.com/en-us/library/az24scfc(v=vs.110).aspx");
+			if(GUILayout.Button("copy escaped pattern"))
+				GUIUtility.systemCopyBuffer = m_RegexPattern.Replace("\\", "\\\\").Replace("\"", "\\\"");
 			GUILayout.EndHorizontal();
 
 			EditorGUI.BeginChangeCheck();
-			pattern = EditorGUILayout.TextArea(pattern, GUILayout.MinHeight(24));
+			m_RegexPattern = EditorGUILayout.TextArea(m_RegexPattern, m_PatternSearchBox);
 			if(EditorGUI.EndChangeCheck())
-				processed = DoRegex(sample, pattern);
+				m_ProcessedText = DoRegex(m_SampleText, m_RegexPattern, m_RegexOptions);
 
-			Color color = GUI.color;
+			GUILayout.Label("Matches (" + m_ProcessedText.matchCount + ")", EditorStyles.boldLabel);
 
-			GUILayout.Label("Matches (" + processed.matchCount + ")", EditorStyles.boldLabel);
+			if( !string.IsNullOrEmpty(m_ProcessedText.error) )
+				EditorGUILayout.HelpBox(m_ProcessedText.error, MessageType.Error);
 
-			if( !string.IsNullOrEmpty(processed.error) )
-				EditorGUILayout.HelpBox(processed.error, MessageType.Error);
+			m_Scroll = GUILayout.BeginScrollView(m_Scroll);
 
-			scroll = GUILayout.BeginScrollView(scroll);
-
-			GUILayout.Label(processed.text, matches_style);
+			RenderText(m_ProcessedText.text, m_MatchesStyle);
 
 			GUILayout.EndScrollView();
-
-			GUILayout.FlexibleSpace();
-
-			GUILayout.Label("Sample Text", EditorStyles.boldLabel);
-
-			sample = EditorGUILayout.TextArea(sample, GUILayout.MinHeight(64), GUILayout.MaxHeight(64));
-
-			GUI.color = color;
-
-#if CUSTOM_FONT
-				EditorStyles.boldLabel.font = old;
-				GUI.skin.font = old;
-#endif
 		}
 
-		static void RenderText(StringBuilder sb, GUIStyle style)
+		void DoContentsAndSettingsGui()
+		{
+			GUILayout.BeginHorizontal();
+			GUILayout.Label("Sample Text", EditorStyles.boldLabel);
+			GUILayout.FlexibleSpace();
+			bool multiLine = (m_RegexOptions & RegexOptions.Multiline) > 0;
+			EditorGUI.BeginChangeCheck();
+			multiLine = EditorGUILayout.Toggle("MultiLine", multiLine);
+			if (EditorGUI.EndChangeCheck())
+			{
+				m_RegexOptions = m_RegexOptions ^ RegexOptions.Multiline;
+				m_ProcessedText = DoRegex(m_SampleText, m_RegexPattern, m_RegexOptions);
+			}
+			GUILayout.EndHorizontal();
+
+			EditorGUI.BeginChangeCheck();
+			m_ContentsScroll = GUILayout.BeginScrollView(m_ContentsScroll);
+			m_SampleText = EditorGUILayout.TextArea(m_SampleText, m_SampleTextStyle, GUILayout.ExpandHeight(true));
+			if (EditorGUI.EndChangeCheck())
+				m_ProcessedText = DoRegex(m_SampleText, m_RegexPattern, m_RegexOptions);
+			GUILayout.EndScrollView();
+
+		}
+
+		static void RenderText(string contents, GUIStyle style)
 		{
 			// need to account for matching html tags
-			// const int MAX_LENGTH = ushort.MaxValue / 2;
-			// int splits = (int) (sb.Length / MAX_LENGTH);
-			// for(int i = 0; i < splits + 1; i++)
-			// {
-			// 	int index = (int) (i * MAX_LENGTH);				
-			// 	GUILayout.Label(sb.ToString(index, System.Math.Min(MAX_LENGTH, sb.Length - index)), style);
-			// }
-			GUILayout.Label(sb.ToString(), style);
+			 const int MAX_LENGTH = ushort.MaxValue / 8;
+			 int splits = (int) (contents.Length / MAX_LENGTH);
+			 for(int i = 0; i < splits + 1; i++)
+			 {
+				int index = (int) (i * MAX_LENGTH);
+				string sub = contents.Substring(index, Math.Min(MAX_LENGTH, contents.Length - index));
+				GUILayout.Label(sub, style);
+			 }
 		}
 
-		static ProcessedText DoRegex(string source, string pattern)
+		static ProcessedText DoRegex(string source, string pattern, RegexOptions options)
 		{
 			ProcessedText processed = new ProcessedText(source, null, 0);
+
+			if (string.IsNullOrEmpty(pattern) || string.IsNullOrEmpty(source))
+				return processed;
 
 			try
 			{
@@ -135,7 +230,7 @@ namespace Parabox.RegexConstructor
 				StringBuilder sb = new StringBuilder(source);
 				bool open = false;
 
-				MatchCollection matches = Regex.Matches(source, pattern);
+				MatchCollection matches = Regex.Matches(source, pattern, options);
 				processed.matchCount = matches.Count;
 
 				for(int i = 0; i < matches.Count; i++)
@@ -145,7 +240,7 @@ namespace Parabox.RegexConstructor
 					if(!open)
 					{
 						sb.Insert(inc + match.Index, "<color=#00FF00FF>");
-						inc += 17;	
+						inc += 17;
 					}
 
 					open = i < matches.Count - 1 && match.Index + match.Length == matches[i+1].Index;
